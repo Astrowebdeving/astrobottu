21a44159a2edfdc04d79aaa96f1a5fa71c06d466
Alex Tu <9000538@student.munster.us>
2023-07-31 17:46:00 -0500

    sds

--- astrobot/settings.py
+++ astrobot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%(cm^*%jk0ql#v3(ram*c2wlu(-k*10-15#*%icw79pgowoe4e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ users = []
 def get_question():
     qs = ''
     id = 1
-    response = requests.get("http://127.0.0.1:8000/api/random/")
+    response = requests.get("https://mysterious-headland-81216.herokuapp.com/api/random/")
     json_data = json.loads(response.text)
     qs += "Question: \n"
     qs+=json_data[0]['title']+"\n"
