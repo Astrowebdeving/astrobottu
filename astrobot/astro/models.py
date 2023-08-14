495cb9a6e3391b6df03b7cd00e03d3781647aa11
Alex Tu <oceania1984ingsoc@yahoo.com>
2023-08-13 21:06:36 -0500

    iwannadie

--- astrobot/astro/models.py
+++ astrobot/astro/models.py
@@ -32,9 +32,8 @@ class Answer(models.Model):
         return self.answer
 
 class Qusers(models.Model):
+    id = models.BigIntegerField(_('id'))
     username = models.CharField(_("username"), max_length = 255)
     totalpoints = models.BigIntegerField(_('totalpoints'), default = 0)
     created_at = models.DateTimeField(_("Created"), auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated"), auto_now=True, auto_now_add=False)
-    def __str__(self):
-        return self.answer
